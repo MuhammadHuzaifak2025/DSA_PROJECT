2f28dba52b8871af2710971831f26de86ca1c3a7
ilyas <ilyas.moiz52@gmail.com>
2023-09-30 22:41:20 +0500

    Update main.cpp

--- Project/main.cpp
+++ Project/main.cpp
@@ -4,8 +4,20 @@
 
 int main(int argc, char *argv[])
 {
+    //This is the MAIN WINDOW FOR SPOTIFY
+    //This is the MAIN WINDOW FOR SPOTIFY
+    //This is the MAIN WINDOW FOR SPOTIFY
+    //This is the MAIN WINDOW FOR SPOTIFY
+    //This is the MAIN WINDOW FOR SPOTIFY
+    //This is the MAIN WINDOW FOR SPOTIFY
+    //This is the MAIN WINDOW FOR SPOTIFY
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
     return a.exec();
+
+    //This is the end of the main window
+    //This is the end of the main window
+    //This is the end of the main window
+    //This is the end of the main window
 }
